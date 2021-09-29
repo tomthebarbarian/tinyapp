74867f5acd1ea9550f7c2ff585026fa75ed3e7f3
Tom Zhang <tom.zhang989@gmail.com>
2021-09-29 19:24:23 -0400

    installed bcrypt for use

--- express_server.js
+++ express_server.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const uuid = require('uuid');
+const bcrypt = require('bcryptjs');
 
 const app = express();
 const PORT = 8080;
